be55ea9a493e340cf886feb6d93f42086ef2ee4e
Floppa <wsanchez_msasd@magnoliascience.org>
2026-09-15 12:00:01 -0700

    Adjust player velocity limits and friction

--- js/player.js
+++ js/player.js
@@ -33,11 +33,11 @@ Player.update = function () {
   // --- 1. decide how fast to go sideways ------------------------------
   if (Input.left)  { Player.vx = Player.vx-CONFIG.MOVE_VELOCITY; }
   if (Input.right) { Player.vx =  Player.vx+CONFIG.MOVE_VELOCITY; }
-  if (Player.vx > 3.5) { Player.vx = 3.5; }
-  if (Player.vx < -3.5) { Player.vx = -3.5 }
+  if (Player.vx > CONFIG.MAX_VELOCITY) { Player.vx = 3.5; }
+  if (Player.vx < -CONFIG.MAX_VELOCITY) { Player.vx = -3.5 }
    // --- 2. Slow down over time
-   Player.vx = Player.vx * 0.95
-  // --- 3. jump, but only if we are standing on something --------------
+   Player.vx = Player.vx * CONFIG.MOVE_FRICTION;
+   // --- 3. jump, but only if we are standing on something --------------
   if (Input.jump && Player.onGround) {
     Player.vy = -CONFIG.JUMP_POWER;   // negative is UP
     Player.onGround = false;

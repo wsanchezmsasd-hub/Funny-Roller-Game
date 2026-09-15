a653df122288a18a6d2c0920aa1ec80e886ada47
Floppa <wsanchez_msasd@magnoliascience.org>
2026-09-15 15:40:33 -0700

    Refactor player movement constants in config.js

--- js/config.js
+++ js/config.js
@@ -18,10 +18,8 @@ var CONFIG = {
   CANVAS_H: 400,
 
   // --- how the player moves -------------------------------------------
-  MOVE_VELOCITY: 0.5,   // velocity per frame left and right
-  MAX_VELOCITY: 8.5,  // How fast the player can move
-  MOVE_FRICTION: 0.75,//how the much the player slows down
-  JUMP_POWER: 10,     // how hard the jump pushes UP. bigger = higher
+  MOVE_SPEED: 0.5,      // pixels per frame left and right
+  JUMP_POWER: 15,     // how hard the jump pushes UP. bigger = higher
   GRAVITY: 0.5,       // how hard the world pulls DOWN. bigger = heavier
   MAX_FALL: 16,       // fastest the player is allowed to fall
 

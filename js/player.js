4180c0f54aa1e2c2f4d9e914b9aa3c1a09957090
Floppa <wsanchez_msasd@magnoliascience.org>
2026-09-16 11:06:49 -0700

    Refactor player movement speed with CONFIG values

--- js/player.js
+++ js/player.js
@@ -31,12 +31,12 @@ Player.update = function () {
   var size = CONFIG.PLAYER_SIZE;
 
   // --- 1. decide how fast to go sideways ------------------------------
-  Player.vx = Player.vx * 0.95; // friction
+  Player.vx = Player.vx * CONFIG.FRICTION; // friction
   if (Input.left)  { Player.vx = Player.vx - CONFIG.MOVE_SPEED; } //speed up left 
   if (Input.right) { Player.vx = Player.vx + CONFIG.MOVE_SPEED; } //speed up right
 
-  if (Player.vx > 8) { Player.vx = 8; }
-  if (Player.vx < -8) { Player.vx = -8; }
+  if (Player.vx > CONFIG.MAX_SPEED) { Player.vx = CONFIG.MAX_SPEED; }
+  if (Player.vx < -CONFIG.MAX_SPEED) { Player.vx = -CONFIG.MAX_SPEED; }
   // --- 2. jump, but only if we are standing on something --------------
   if (Input.jump && Player.onGround) {
     Player.vy = -CONFIG.JUMP_POWER;   // negative is UP

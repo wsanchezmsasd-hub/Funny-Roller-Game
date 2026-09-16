d290df764afe9f4eb25985e088bb256064188ed5
Floppa <wsanchez_msasd@magnoliascience.org>
2026-09-16 11:09:03 -0700

    Remove MAX_SPEED and FRICTION from config.js
    Removed MAX_SPEED and FRICTION constants from config.

--- js/config.js
+++ js/config.js
@@ -19,8 +19,6 @@ var CONFIG = {
 
   // --- how the player moves -------------------------------------------
   MOVE_SPEED: 0.25,      // pixels per frame left and right
-  MAX_SPEED: 8,
-  FRICTION: 0.75,
   JUMP_POWER: 15,     // how hard the jump pushes UP. bigger = higher
   GRAVITY: 0.5,       // how hard the world pulls DOWN. bigger = heavier
   MAX_FALL: 16,       // fastest the player is allowed to fall

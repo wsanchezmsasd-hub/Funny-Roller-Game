ae06b0e2b9422d392fa38b3943ea492be57dd22e
Floppa <wsanchez_msasd@magnoliascience.org>
2026-09-16 11:07:38 -0700

    Reduce MOVE_SPEED and add MAX_SPEED and FRICTION

--- js/config.js
+++ js/config.js
@@ -18,7 +18,9 @@ var CONFIG = {
   CANVAS_H: 400,
 
   // --- how the player moves -------------------------------------------
-  MOVE_SPEED: 0.5,      // pixels per frame left and right
+  MOVE_SPEED: 0.25,      // pixels per frame left and right
+  MAX_SPEED: 8,
+  FRICTION: 0.75,
   JUMP_POWER: 15,     // how hard the jump pushes UP. bigger = higher
   GRAVITY: 0.5,       // how hard the world pulls DOWN. bigger = heavier
   MAX_FALL: 16,       // fastest the player is allowed to fall

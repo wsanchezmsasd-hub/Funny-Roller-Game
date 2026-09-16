eb9e7b0dd5b884486a2ab3defa59af2c30c7d458
Floppa <wsanchez_msasd@magnoliascience.org>
2026-09-16 11:19:04 -0700

    Modify player movement dynamics
    Increase friction effect and adjust velocity limits.

--- js/player.js
+++ js/player.js
@@ -31,12 +31,17 @@ Player.update = function () {
   var size = CONFIG.PLAYER_SIZE;
 
   // --- 1. decide how fast to go sideways ------------------------------
-  Player.vx = Player.vx * 0.75; // friction
+
+  var size = CONFIG.PLAYER_SIZE;
+
+  // --- 1. decide how fast to go sideways ------------------------------
+  Player.vx = Player.vx * 0.95; // friction
   if (Input.left)  { Player.vx = Player.vx - CONFIG.MOVE_SPEED; } //speed up left 
   if (Input.right) { Player.vx = Player.vx + CONFIG.MOVE_SPEED; } //speed up right
 
-  if (Player.vx > 6) { Player.vx = 6; }
-  if (Player.vx < -6) { Player.vx = -6; }
+  if (Player.vx > 8) { Player.vx = 8; }
+  if (Player.vx < -8 { Player.vx = -8; }
+
   // --- 2. jump, but only if we are standing on something --------------
   if (Input.jump && Player.onGround) {
     Player.vy = -CONFIG.JUMP_POWER;   // negative is UP
